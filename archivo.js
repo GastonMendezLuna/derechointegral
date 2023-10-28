0ee0b4a594fcaec016b4bbe8518d420b6329cfe0
GastonMendezLuna <mendezlunagaston@gmail.com>
2023-10-28 12:39:38 -0300

    nueva rama

---
+++ archivo.js
@@ -0,0 +1,19 @@
+let numero = 7;
+let resultado= "";
+
+switch (numero) {
+case 1:
+      resultado = "El numero es 1";
+    break;
+    case 5:
+        resultado = "El numero es 5";
+        break;
+        case 7:
+            resultado = "El numero es 7";
+            break;
+            default:
+                resultado = "El numero";
+                break;
+
+}
+console.log(resultado)
